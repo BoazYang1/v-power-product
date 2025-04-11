d90f8f947f5e89aa6b032f3cda15c5365e839374
BoazYang1 <93645681+BoazYang1@users.noreply.github.com>
2025-04-11 10:58:17 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -674,7 +674,7 @@ export default function Home() {
                   >
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                   </svg>
-                  客服电话：400-123-4567
+                  客服电话：177......
                 </p>
                 <p className="text-gray-600 flex items-center gap-2">
                   <svg
@@ -692,7 +692,7 @@ export default function Home() {
                     <rect width="20" height="16" x="2" y="4" rx="2" />
                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                   </svg>
-                  邮箱：service@weishen.com
+                  邮箱：XXX@XXXX
                 </p>
                 <p className="text-gray-600 flex items-center gap-2">
                   <svg
@@ -710,7 +710,7 @@ export default function Home() {
                     <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
                     <circle cx="12" cy="10" r="3" />
                   </svg>
-                  地址：广州市天河区科技园
+                  地址：保密
                 </p>
               </div>
             </div>
